c5a0c627ba4c920b1b96830e799a546f9913c7c1
PranauvS <pranauvs.19it@kongu.edu>
2022-05-15 11:30:51 +0530

    added contents and form templates in about, login and register page

--- sivaminfotech/src/app/app-routing.module.ts
+++ sivaminfotech/src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ServicesComponent } from './services/services.component';
 import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+
 const routes: Routes = [{path:'login',component:LoginComponent},
+{path:'register',component:RegisterComponent},
 {path:'home',component:HomeComponent},
 {path:'services',component:ServicesComponent},
 {path:'about',component:AboutComponent}
