fe8c37ac5c5dfb17faa91098efb4dc40be552561
PranauvS <pranauvs.19it@kongu.edu>
2022-05-16 18:52:20 +0530

    added footer

--- sivaminfotech/src/app/home/home.component.ts
+++ sivaminfotech/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+
 
 @Component({
   selector: 'app-home',
